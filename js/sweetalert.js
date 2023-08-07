ea3c3e9ea44adb6fd757ffd0de0cbc058e4844fe
Pola2505 <sandra27popielarczyk@gmail.com>
2023-08-06 21:09:07 -0300

    sweetalert

--- js/sweetalert.js
+++ js/sweetalert.js
@@ -1,13 +1,26 @@
 const sweetAlert = document.querySelector("#sweetAlert");
+const contenedorFormulario = document.querySelector("#contenedor-form");
+const mandarForm = document.querySelector("#mandar-form");
 
-sweetAlert.addEventListener("click", (e) => {
+sweetAlert.addEventListener("click", mandarFormulario);
+
+function mandarFormulario(e){
     e.preventDefault();
     Swal.fire({
-        title: 'Error!',
-        text: 'Do you want to continue',
-        icon: 'error',
-        confirmButtonText: 'Cool'
+        
+        title: '¿Quieres enviar el formulario?',
+        icon: 'question',
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText:'Si',
+        cancelButtonText:'No',
+        
+      }).then((result) => {
+        if (result.isConfirmed){
+            contenedorFormulario.classList.add("inactive");
+            mandarForm.classList.remove("inactive");
+        }
       })
-
-})
-
+      
+}
